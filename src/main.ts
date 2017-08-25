0f221f22d668c174a9524fbb36eb4eaf32bb6193
JiaLi.Passion <JiaLi.Passion@gmail.com>
2017-08-25 11:02:43 +0900

    add test

--- src/main.ts
+++ src/main.ts
@@ -8,4 +8,30 @@ if (environment.production) {
   enableProdMode();
 }
 
+const desc = Object.getOwnPropertyDescriptor(window, 'onbeforeunload');
+if (desc) {
+    const oriGet = desc.get;
+    const oriSet = desc.set;
+    const preDefinedEvent = document.createEvent('Event');
+    preDefinedEvent.initEvent('beforeunload', false, true);
+    Object.defineProperty(window, 'onbeforeunload', {
+        configurable: true,
+        get: function () {
+          const wrapFn = oriGet.apply(this, arguments);
+          if (wrapFn) {
+            return function() {
+              const args = Array.prototype.slice.call(arguments);
+              let evt = args.length === 0 ? preDefinedEvent : args[0];
+              return wrapFn.apply(this, [evt]);
+            }
+          }
+          return wrapFn;
+        }
+    });
+}
+
+window.onbeforeunload = function () {
+    console.log('a');
+};
+
 platformBrowserDynamic().bootstrapModule(AppModule);
