85b560ada98c5a3978fbafe478983189151a2c8f
Nasrullo Khojayori <nkhodjaerov@transparent.com>
2017-08-24 21:42:22 -0400

    Remove unused files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,8 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    template: '<div style="height: 40px">Test</div>'
 })
 export class AppComponent {
-  title = 'app';
 }
